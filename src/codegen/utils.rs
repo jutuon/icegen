1661a00b0a47d9aa9ed011b8b54cb70dacc05a5a
Juuso Tuononen <jutuon@outlook.com>
2026-01-28 17:46:00 +0200

    Don't indent empty lines

--- src/codegen/utils.rs
+++ src/codegen/utils.rs
@@ -2,7 +2,13 @@ pub fn indent_lines(indentation: &str, input: impl AsRef<str>) -> String {
     input
         .as_ref()
         .lines()
-        .map(|line| format!("{}{}", indentation, line))
+        .map(|line| {
+            if line.trim().is_empty() {
+                String::new()
+            } else {
+                format!("{}{}", indentation, line)
+            }
+        })
         .collect::<Vec<String>>()
         .join("\n")
 }
